f913157c6237e0e886ab070d6fbc9518aaf2b0ea
Hana Song <may+github@maymeow.click>
2017-10-26 15:33:01 +0200

    update

--- docker-entry.sh
+++ docker-entry.sh
@@ -5,6 +5,7 @@ chmod 777 -R /var/www/html/tmp
 chmod 777 -R /var/www/html/logs
 chmod 777 -R /var/www/html/datastore
 chmod 777 -R /var/www/html/webroot/data
+chmod 777 -R /var/opt/cakeapp/data
 chmod +x /var/www/html/bin/cake
 
 echo "Wait until database $MYSQL_HOST:3306 is ready..."
